0d05c3929264b4f10dd753465d1a6d01a9912015
Edwina <z5165813@unsw.edu.au>
2021-03-09 19:38:35 +1100

    Week 4 tutorial

---
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/CountryAdapter.java
@@ -0,0 +1,70 @@
+package au.edu.unsw.infs3634.covidtracker;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.CountryViewHolder> {
+    public ArrayList<Country> mCountries;
+    private RecyclerViewClickListener mListener;
+
+    public CountryAdapter(ArrayList<Country> countries, RecyclerViewClickListener listener) {
+        mCountries = countries;
+        mListener = listener;
+
+    }
+
+    public interface RecyclerViewClickListener {
+        void onClick(View view, String id);
+    }
+
+    @NonNull
+    @Override
+    public CountryAdapter.CountryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.country_list, parent, false);
+        return new CountryViewHolder(v, mListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CountryAdapter.CountryViewHolder holder, int position) {
+        Country country = mCountries.get(position); //this takes in position so i wan to get a country form my lsit of countries, i want t oget the one tht is relevant in the position of that recycler view. How i do that is: get(index from country list-
+        holder.rvcountry.setText(country.getCountry());
+        holder.rvTotalCases.setText(String.valueOf(country.getTotalConfirmed()));
+        holder.rvNewCases.setText(String.valueOf(country.getNewConfirmed()));
+        holder.itemView.setTag(country.getId()); //make sure this is getId or the whole thing will fail!
+
+
+    }
+
+    @Override
+    public int getItemCount() { //its going to tell the adapter how many items it iwll need to make viewholder for if return 0 in next line it says currently need to make 0.
+//        return 0;
+        return mCountries.size();
+    }
+
+    public class CountryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView rvcountry, rvTotalCases, rvNewCases;   //used to create each one of hte rows
+        private CountryAdapter.RecyclerViewClickListener btlistener;
+
+        public CountryViewHolder(@NonNull View itemView, CountryAdapter.RecyclerViewClickListener listener) {
+            super(itemView);
+            this.btlistener = listener;
+            itemView.setOnClickListener(this);
+            rvcountry = itemView.findViewById(R.id.rvCountry);
+            rvTotalCases = itemView.findViewById(R.id.rvTotalCases);
+            rvNewCases = itemView.findViewById(R.id.rvNewCases);
+        }
+
+        @Override
+        public void onClick(View v) {
+            btlistener.onClick(v, (String) v.getTag());
+        }
+    }
+}
--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
@@ -1,6 +1,8 @@
 package au.edu.unsw.infs3634.covidtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,19 +10,37 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private CountryAdapter mAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.btLaunchActivity);
-        button.setOnClickListener(new View.OnClickListener() {
+        mRecyclerView = findViewById(R.id.rvlist);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        CountryAdapter.RecyclerViewClickListener mlistener = new CountryAdapter.RecyclerViewClickListener() { //thisis our onClick method, when we click a method in our rv what we want to happen is written below
             @Override
-            public void onClick(View v) {
-                launchDetailActivity("40735414-f500-4ba6-a05f-375fb7e93db8");
+            public void onClick(View view, String id) {
+                launchDetailActivity(id); //we want to pass to detail activilty this id so they know which one to open
             }
-        });
+        };
+        //no we need to initialise the adapter
+        mAdapter = new CountryAdapter(Country.getCountries(), mlistener); //bc in other page we have constructor which passed in array and something else in other java file
+        mRecyclerView.setAdapter(mAdapter);
+//        DELETED ALL THIS
+//        Button button = findViewById(R.id.btLaunchActivity);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                launchDetailActivity("40735414-f500-4ba6-a05f-375fb7e93db8");
+//            }
+//        }); DELETED
     }
 
     private void launchDetailActivity(String message) {
